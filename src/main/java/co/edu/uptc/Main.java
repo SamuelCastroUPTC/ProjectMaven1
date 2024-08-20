aa8a7ffcefb6b59c3bd73c43ebe615f0388309da
Samuel <sacapo2112@gmail.com>
2024-08-20 17:27:28 -0500

    Mi primer proyecto con maven

---
+++ src/main/java/co/edu/uptc/Main.java
@@ -0,0 +1,11 @@
+package co.edu.uptc;
+
+import co.edu.uptc.models.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person person = new Person();
+        person.setName("John");
+    }
+}
