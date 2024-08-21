a241f58717d3648db220165171650bf308115ddf
Samuel <sacapo2112@gmail.com>
2024-08-21 10:29:11 -0500

    se quito linea basura

--- src/main/java/co/edu/uptc/Main.java
+++ src/main/java/co/edu/uptc/Main.java
@@ -6,10 +6,8 @@ import co.edu.uptc.views.frame;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Person person = new Person();
         person.setName("John");
-        System.out.println(person.getName());
         frame frame = new frame();
         btnCenter btnCenter = new btnCenter();
     }
